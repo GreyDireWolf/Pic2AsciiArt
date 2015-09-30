70d8d355fd71605761be34bce01e03564910ea1e
Amey <ameynarkhede02@gmail.com>
2015-09-30 19:00:29 +0530

    Support python 2 to python 3

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pic2AsciiArt
+script to convert picture to ascii type art
+
--- main.py
+++ main.py
@@ -2,31 +2,17 @@
 # -*- coding: utf-8 -*-
 
 
-"""
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                    Version 2, December 2004
-
- Copyright (C) 2013 tuurngait <tuurngait@icloud.com>
-
- Everyone is permitted to copy and distribute verbatim or modified
- copies of this license document, and changing it is allowed as long
- as the name is changed.
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-  0. You just DO WHAT THE FUCK YOU WANT TO.
-"""
 
 
-__title__ = 'pic2ascii'
-__version__ = '0.2'
-__author__ = 'tuurngait'
-__license__ = 'Do What The Fuck You Want To Public License'
-__copyright__ = '(c) 2013, tuurngait <tuurngait@icloud.com>'
+from __future__ import absolute_import
+from itertools import imap
+__title__ = u'Pic2AsciiArt'
+__version__=0.1
 
 
-FONT = "fonts/FSEX300.ttf"
+
+
+FONT = u"fonts/FSEX300.ttf"
 FONTSIZE = 15
 CELLWIDTH = 10
 CELLHEIGHT = 14
@@ -37,8 +23,6 @@ import sys
 import os
 import time
 
-from functools import reduce
-
 from PIL import (
     Image, ImageFont, ImageDraw, ImageFilter
 )
@@ -46,58 +30,58 @@ from PIL import (
 import charset
 
 
-def timer (f: callable) -> callable:
-    def w (*a: list, **kw: dict) -> object:
+def timer (f):
+    def w (*a, **kw):
         s = time.time()
         r = f(*a, **kw)
-        print("{}{} ++> {:g} secs.".format(f.__name__, a, time.time() - s))
+        #print u"{}{} ++> {:g} secs.".format(f.__name__, a, time.time() - s)
         return r
     return w
 
 
-def usage () -> None:
+def usage ():
     message = (
-        '=======================================================================\n'
-        '{app} version {ver}\n\n'
-        'Usage:\n'
-        '{0} <source file> <width> [bg brightness] [bg color] [output file]\n\n'
-        'Where:\n'
-        '\t<source file>: path to your image file\n'
-        '\t<width>: desired output picture width: (int) from [2 - +Inf)\n'
-        '\t[bg brightness]: bg brightness: (int) from [0; 255]\n'
-        '\t[bg color]: bg color: e.g. "black" or "white"\n'
-        '\t[output file]: path to output file\n\n'
+        u'=======================================================================\n'
+        u'{app} version {ver}\n\n'
+        u'Usage:\n'
+        u'{0} <source file> <width> [bg brightness] [bg color] [output file]\n\n'
+        u'Where:\n'
+        u'\t<source file>: path to your image file\n'
+        u'\t<width>: desired output picture width: (int) from [2 - +Inf)\n'
+        u'\t[bg brightness]: bg brightness: (int) from [0; 255]\n'
+        u'\t[bg color]: bg color: e.g. "black" or "white"\n'
+        u'\t[output file]: path to output file\n\n'
         # TODO: font
         # TODO: fontsize
-        '======================================================================='
+        u'======================================================================='
     ).format(sys.argv[0], ver=__version__, app=__title__)
-    print(str(message), file=sys.stderr)
+    print >>sys.stderr, unicode(message)
     sys.exit(1)
 
 
-def spinCursor () -> str:
-    cursor = r'-\|/'
+def spinCursor ():
+    cursor = '-\|/'
     i = 0
     while True:
         yield cursor[i]
         i = (i + 1) % cursor.__len__()
 
 
-def bgColor (source: Image, w: int, h: int, br: int) -> (int, int, int):
-    for i in range(0, w):
-        for j in range(0, h):
-            yield tuple(map((lambda x: abs(x-br) if x > br else 0), list(source.getpixel((i, j)))))
+def bgColor (source, w, h, br):
+    for i in xrange(0, w):
+        for j in xrange(0, h):
+            yield tuple(imap((lambda x: abs(x-br) if x > br else 0), list(source.getpixel((i, j)))))
 
 
-def fontColor (source: Image, w: int, h: int) -> ((int, int, int), int):
-    for i in range(0, w):
-        for j in range(0, h):
+def fontColor (source, w, h):
+    for i in xrange(0, w):
+        for j in xrange(0, h):
             pxColor = source.getpixel((i, j))
             yield (pxColor, (reduce(lambda x, y: x+y, pxColor) // 3 * (charset.total - 1)) // 255)
 
 
 @timer
-def main () -> None:
+def main ():
     # TODO: use optparse or argparse or optparse or smth else
     if len(sys.argv) < 2:
         usage()
@@ -111,27 +95,27 @@ def main () -> None:
         try:
             backgroundColor = sys.argv[4]
         except:
-            backgroundColor = "black"
+            backgroundColor = u"black"
         try:
             outputFilePath = sys.argv[5]
         except:
-            outputFilePath = sourceImagePath + ".(ASCII).png"
+            outputFilePath = sourceImagePath + u".(ASCII).png"
 
     try:
         sourceImage = Image.open(sourceImagePath)
-        print("Loaded image with size:", sourceImage.size)
+        print "Loaded image with size:", sourceImage.size
     except:
-        print("An error has occured while opening image...", file=sys.stderr)
+        print >>sys.stderr, u"An error has occured while opening image..."
         exit(1)
 
     outputHeight = (outputWidth * sourceImage.size[1]) // sourceImage.size[0]
 
-    background = Image.new("RGBA", (outputWidth, outputHeight), "white")
+    background = Image.new(u"RGBA", (outputWidth, outputHeight), u"white")
     sourceImage = sourceImage.filter(ImageFilter.SHARPEN).resize((outputWidth, outputHeight), Image.ANTIALIAS)
-    sourceImage = Image.alpha_composite(background, sourceImage).convert("RGB")
+    sourceImage = Image.alpha_composite(background, sourceImage).convert(u"RGB")
 
     # sourceImage.show()
-    print("Output image size in chars: ({w}, {h})".format(w=outputWidth, h=outputHeight))
+    print u"Output image size in chars: ({w}, {h})".format(w=outputWidth, h=outputHeight)
 
     font = ImageFont.truetype(FONT, FONTSIZE)
     wStep = CELLWIDTH
@@ -141,26 +125,26 @@ def main () -> None:
     outputWidth *= wStep
     outputHeight *= hStep
 
-    outputImage = Image.new("RGB", (outputWidth, outputHeight), backgroundColor)
-    print("Output image size: ({w}, {h})".format(w=outputWidth, h=outputHeight))
+    outputImage = Image.new(u"RGB", (outputWidth, outputHeight), backgroundColor)
+    print u"Output image size: ({w}, {h})".format(w=outputWidth, h=outputHeight)
 
     draw = ImageDraw.Draw(outputImage)
-    print("Using charset with {} chars total".format(charset.total))
-    print("File will be saved to: {x}".format(x=os.getcwd()), "\nProcessing...")
+    print u"Using charset with {} chars total".format(charset.total)
+    print u"File will be saved to: {x}".format(x=os.getcwdu()), u"\nProcessing..."
 
     # spinner = spinCursor()
-    for i in range(0, outputWidth, wStep):
-        for j in range(0, outputHeight, hStep):
+    for i in xrange(0, outputWidth, wStep):
+        for j in xrange(0, outputHeight, hStep):
             # print(spinner.__next__(), end='\b')
-            draw.rectangle([i, j, i+wStep, j+hStep], "rgb{}".format(bgClrGen.__next__()))
-            charClr = fntClrGen.__next__()
-            draw.text((i+1, j-1), charset.chars[int(charClr[1])], fill="rgb{}".format(charClr[0]), font=font)
+            draw.rectangle([i, j, i+wStep, j+hStep], u"rgb{}".format(bgClrGen.next()))
+            charClr = fntClrGen.next()
+            draw.text((i+1, j-1), charset.chars[int(charClr[1])], fill=u"rgb{}".format(charClr[0]), font=font)
 
     # print('\b', end='\b')
 
-    outputImage.save(outputFilePath, "PNG")
-    print("Converted & saved successfully to: {o}".format(o=outputFilePath))
+    outputImage.save(outputFilePath, u"PNG")
+    print u"Converted & saved successfully to: {o}".format(o=outputFilePath)
 
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     main()
